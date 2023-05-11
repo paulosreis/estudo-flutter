1464d984009820359131cc25dba821fb0eb6a452
Paulo G Reis <paulosantos--@live.com>
2023-05-10 22:16:03 -0400

    refactor: passa a pontuação como metodo ao chamar onPressed

--- projeto_quiz/lib/questionario.dart
+++ projeto_quiz/lib/questionario.dart
@@ -6,7 +6,7 @@ import './resposta.dart';
 class Questionario extends StatelessWidget {
   final List<Map<String, Object>> perguntas;
   final int perguntaSelecionada;
-  final void Function() quandoResponder;
+  final void Function(int) quandoResponder;
 
   Questionario({
     required this.perguntas,
@@ -27,9 +27,12 @@ class Questionario extends StatelessWidget {
     return Column(
       children: <Widget>[
         Questao(perguntas[perguntaSelecionada]['texto'].toString()),
-        ...respostas
-            .map((resp) => Resposta(resp['texto'] as String, quandoResponder))
-            .toList(),
+        ...respostas.map((resp) {
+          return Resposta(
+            resp['texto'].toString(),
+            () => quandoResponder(int.parse(resp['pontuacao'].toString())),
+          );
+        }).toList(),
       ],
     );
   }
