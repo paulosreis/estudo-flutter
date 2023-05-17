875eac7c3ce222c7a2a5580342c8cedd037b455d
Paulo G Reis <paulosantos--@live.com>
2023-05-17 19:25:52 -0400

    feat: cria funionalidade de reiniciar o quiz

--- projeto_quiz/lib/main.dart
+++ projeto_quiz/lib/main.dart
@@ -38,6 +38,13 @@ class _PerguntaAppState extends State<PerguntaApp> {
     }
   ];
 
+  void _reiniciarQuestionario() {
+    setState(() {
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
+    });
+  }
+
   bool get temPerguntaSelecionada {
     return _perguntaSelecionada < _perguntas.length;
   }
@@ -70,7 +77,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                 perguntas: _perguntas,
                 perguntaSelecionada: _perguntaSelecionada,
                 quandoResponder: _responder)
-            : Resultado(_pontuacaoTotal),
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
       ),
     );
   }
